a40967909daf62f2d756564ea61513d1efbce1a0
Jim Toth <jim@meme.coach>
2020-04-22 21:25:32 -0400

    Nav bar & placeholder home page

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,59 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        venenatis consectetur tortor, id ornare elit feugiat eget. Quisque
+        laoreet, augue a ultricies tempor, augue sapien vestibulum turpis, vel
+        vulputate enim ipsum eget leo. Nulla facilisi. Proin magna felis,
+        viverra ac purus eu, luctus vulputate ligula. Vestibulum magna nulla,
+        placerat vel viverra vel, condimentum et sapien. Suspendisse faucibus
+        enim est, a consectetur erat ullamcorper quis. Sed tincidunt ut sapien
+        sit amet convallis. Proin consectetur diam metus, cursus elementum dui
+        commodo at. Vestibulum sed semper lorem, quis commodo neque. Praesent
+        eleifend dignissim finibus.
+      </p>
+      <p>
+        Pellentesque vel iaculis augue, vel consequat dui. Aliquam sed semper
+        metus. Sed sagittis eleifend urna id eleifend. Aenean eget tempus est.
+        Integer pellentesque, nunc eu pulvinar tempor, est est consectetur ex,
+        posuere rutrum nisl lacus quis risus. Quisque interdum semper felis, vel
+        fringilla diam placerat vitae. Morbi sagittis gravida ipsum eget congue.
+        Vivamus quis consequat nibh, id rutrum arcu. Donec ornare risus sit amet
+        volutpat porta.
+      </p>
+      <p>
+        Maecenas pretium elit nec est ornare, id euismod erat interdum. Quisque
+        aliquam turpis nisl, non lacinia diam consequat sit amet. Proin
+        tincidunt mi ut erat scelerisque porta. Proin feugiat enim a aliquam
+        lacinia. Nulla congue lacus ut mi bibendum imperdiet. Fusce laoreet
+        libero risus, at euismod nunc tincidunt non. Vestibulum ac neque nulla.
+        Morbi ullamcorper nisi a turpis sagittis, nec iaculis nisi convallis.
+        Proin id risus et urna posuere condimentum. Fusce sit amet ornare metus,
+        eu posuere velit.
+      </p>
+      <p>
+        Suspendisse non quam diam. Duis sagittis sagittis vestibulum.
+        Pellentesque porttitor est eu nisl bibendum cursus in mattis lacus.
+        Quisque scelerisque purus sed neque facilisis, a interdum ante
+        dignissim. Cras bibendum mi mauris, eu imperdiet sapien vestibulum id.
+        Aenean eget molestie sem. Sed fringilla sit amet felis eu porttitor. Ut
+        ut malesuada nunc, ac interdum enim. Maecenas orci lectus, fringilla et
+        sem sed, posuere placerat nunc. Integer scelerisque eu lorem interdum
+        dignissim. Ut vehicula neque aliquam sem consequat fringilla.
+      </p>
+      <p>
+        Nullam vehicula dolor orci, et venenatis nibh sollicitudin vitae. Nunc
+        urna magna, tincidunt in pulvinar semper, gravida eget augue. Donec ac
+        maximus nulla, eu rhoncus turpis. Cras luctus at lectus et iaculis.
+        Aliquam efficitur metus vel dui consectetur rutrum. Aliquam vel mollis
+        metus. Fusce ullamcorper risus magna, eu tristique quam scelerisque
+        vitae. Suspendisse eu erat scelerisque, sagittis quam quis, scelerisque
+        ligula. Pellentesque ex nulla, iaculis eu vulputate in, interdum posuere
+        augue. Proin a orci a odio lacinia sagittis id et elit. Praesent metus
+        nunc, volutpat quis magna vel, dignissim rutrum est. Ut vitae quam sed
+        odio venenatis bibendum a at lectus.
+      </p>
     </v-flex>
   </v-layout>
 </template>
