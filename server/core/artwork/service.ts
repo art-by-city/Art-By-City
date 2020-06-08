004fe6f46bdc6c1e4895fedbb1173989628e0ac6
Jim Toth <jim@meme.coach>
2020-06-08 08:41:45 -0400

    Adds option to hydrate domain objectswhen fetching with domain service

--- server/core/artwork/service.ts
+++ server/core/artwork/service.ts
@@ -2,8 +2,9 @@ import { injectable, inject } from 'inversify'
 
 import ValidationError from '../api/errors/validationError'
 import { User, UserRepository } from '../user'
+import { DomainServiceOptions } from '../domainService.interface'
 import ArtworkValidator from './validator'
-import { Artwork, ArtworkImage, ArtworkService, ArtworkRepository } from './'
+import { Artwork, ArtworkService, ArtworkRepository } from './'
 
 @injectable()
 export default class ArtworkServiceImpl implements ArtworkService {
@@ -21,29 +22,20 @@ export default class ArtworkServiceImpl implements ArtworkService {
     this.userRepository = userRepository
   }
 
-  async create(
-    user: User,
-    artwork: Artwork,
-    images: ArtworkImage[]
-  ): Promise<Artwork | null> {
-    // Validate
+  async create(artwork: Artwork): Promise<Artwork | null> {
     const messages = this.artworkValidator.validate(artwork)
 
     if (messages) {
       throw new ValidationError(messages)
     }
 
-    artwork.owner = this.userRepository.getDocumentReference(user.id)
-    artwork.images = images
-
-    // Save
     return await this.artworkRepository.create(artwork)
   }
 
-  async get(id: string): Promise<Artwork | null> {
+  async get(id: string, opts?: DomainServiceOptions): Promise<Artwork | null> {
     const artwork = await this.artworkRepository.get(id)
 
-    if (artwork) {
+    if (artwork && opts?.hydrated) {
       artwork.owner = <User>await this.userRepository.get(artwork?.owner.id)
     }
 
@@ -70,15 +62,13 @@ export default class ArtworkServiceImpl implements ArtworkService {
 
     const artworks = await this.artworkRepository.find(filter)
 
-    const hydratedArtworks = Promise.all(
+    return Promise.all(
       artworks.map(async (a) => {
         a.owner = <User>await this.userRepository.get(a.owner.id)
 
         return a
       })
     )
-
-    return hydratedArtworks
   }
 
   async listLikedByUser(user: User): Promise<Artwork[]> {
@@ -89,15 +79,13 @@ export default class ArtworkServiceImpl implements ArtworkService {
 
     const artworks = await this.artworkRepository.find(filter)
 
-    const hydratedArtworks = Promise.all(
+    return Promise.all(
       artworks.map(async (a) => {
         a.owner = <User>await this.userRepository.get(a.owner.id)
 
         return a
       })
     )
-
-    return hydratedArtworks
   }
 
   update(artwork: Artwork): Promise<Artwork> {
