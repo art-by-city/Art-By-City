8be708e9812636e1f08b379af651e01de89af23b
Jim Toth <jim@meme.coach>
2020-06-08 13:43:15 -0400

    Remove console log

--- components/artwork/ArtworkExplorer.component.vue
+++ components/artwork/ArtworkExplorer.component.vue
@@ -118,7 +118,6 @@ export default class ArtworkExplorer extends Vue {
   }
 
   onResize() {
-    console.log('onResize() window.innerHeight', window.innerHeight)
     this.vw = window.innerWidth
   }
 
