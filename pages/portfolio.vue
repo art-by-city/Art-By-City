f39145baca1d6881bb3760fec448b7a5fe0ff1a6
bryanmoon1991 <bryanmoon1991@gmail.com>
2021-04-10 11:06:02 -0400

    reverts portfolio page back to master version

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -6,82 +6,17 @@
         Upload New Artwork
       </nuxt-link>
       <v-container fluid>
-        <v-text-field
-          class="mb-2"
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-        ></v-text-field>
-        <v-row>
-          <!-- cols should change depending on breakpoints, but i dont know where to set them -->
-          <v-col v-for="(artwork, i) in artworks" :key="i" cols="4">
-            <v-lazy transition="fade-transition">
-              <template>
-                <v-card>
-                  <v-img
-                    :src="
-                      $config.imgBaseUrl +
-                        '/artwork-images/' +
-                        artwork.images[0].source
-                    "
-                  >
-                    <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                      <v-spacer></v-spacer>
-                      <v-speed-dial
-                        v-model="fab"
-                        :direction="direction"
-                        :transition="transition"
-                      >
-                        <template v-slot:activator>
-                          <v-btn color="white" icon v-model="fab" fab>
-                            <v-icon>mdi-dots-vertical</v-icon>
-                          </v-btn>
-                        </template>
-                        <v-btn fab dark small color="green">
-                          <v-icon>mdi-pencil</v-icon>
-                        </v-btn>
-                        <v-btn fab dark small color="indigo">
-                          <v-icon>mdi-plus</v-icon>
-                        </v-btn>
-                        <v-btn fab dark small color="red">
-                          <v-icon>mdi-delete</v-icon>
-                        </v-btn>
-                      </v-speed-dial>
-                    </v-app-bar>
-                  </v-img>
-
-                  <v-card-text>
-                    <h2>
-                      {{ artwork.title }}
-                    </h2>
-                    <v-icon v-if="artwork.published">
-                      eye-tick-outline
-                    </v-icon>
-                    <v-icon v-else>eye-remove-outline</v-icon>
-                    <!-- content -->
-                  </v-card-text>
-                </v-card>
-              </template>
-            </v-lazy>
-          </v-col>
-        </v-row>
-        <!-- <v-data-table :headers="headers" :items="artworks" item-key="id">
+        <v-data-table :headers="headers" :items="artworks" item-key="id">
           <template v-slot:item.likes="{ item }">
             {{ totalLikes(item) }}
-            {{ debug(item) }}
-          </template>
-          <template v-slot:item.published="{ item }">
-            <v-icon>{{
-              item ? 'eye-tick-outline' : 'eye-remove-outline'
-            }}</v-icon>
           </template>
           <template v-slot:item.images="{ item }">
             <v-img
               :src="
                 $config.imgBaseUrl + '/artwork-images/' + item.images[0].source
               "
-              width="200"
-              height="200"
+              width="100"
+              height="100"
             ></v-img>
           </template>
           <template v-slot:item.actions="{ item }">
@@ -89,19 +24,8 @@
               <v-icon>mdi-eye</v-icon>
             </v-btn>
           </template>
-        </v-data-table> -->
+        </v-data-table>
       </v-container>
-      <v-btn
-        color="blue darken-2"
-        nuxt
-        to="/a/new"
-        dark
-        class="text-lowercase upload"
-      >
-        <v-icon>
-          mdi-plus
-        </v-icon>
-      </v-btn>
     </v-layout>
   </div>
 </template>
@@ -119,22 +43,13 @@ export default class UserArtworkPage extends PageComponent {
   artworks: any[] = []
   cities: any[] = []
 
-  data() {
-    return {
-      direction: 'bottom',
-      fab: false,
-      transition: 'slide-y-transition'
-    }
-  }
-
   headers = [
     { text: 'title', value: 'title' },
-    { text: 'published', value: 'published' },
-    { text: 'images', value: 'images' },
     { text: 'type', value: 'type' },
     { text: 'city', value: 'city' },
     { text: 'hashtags', value: 'hashtags' },
     { text: 'likes', value: 'likes' },
+    { text: 'images', value: 'images' },
     { text: 'actions', value: 'actions' }
   ]
 
@@ -171,15 +86,5 @@ export default class UserArtworkPage extends PageComponent {
       }
     }
   }
-
-  debug(item: any) {
-    console.log('debugger:', item)
-  }
 }
 </script>
-
-<style>
-.upload {
-  position: sticky;
-}
-</style>
