6412bb8d2e8596f2a4a3a506725436415a884b59
Jim Toth <jim@meme.coach>
2021-08-13 16:13:10 -0400

    Fix app config

--- services/artwork/service.ts
+++ services/artwork/service.ts
@@ -17,7 +17,7 @@ export default class ArtworkService {
     this.transactionBuilder = new TransactionBuilder(
       this.$arweave,
       this.$ardb,
-      context.$config.arweave.apiConfig
+      context.$config.arweave.appConfig
     )
   }
 
