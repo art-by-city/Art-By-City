bd6ac5caf7bd208645580a578c2b35931982f073
Jim Toth <jim@meme.coach>
2021-08-24 08:29:55 -0400

    Embeds mailing list signup google form

--- pages/register.vue
+++ pages/register.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="register-page">
-    <h1>Register</h1>
-    <h3>TODO</h3>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeBdtAoUMd5HY2K26Vq8VUdPIFL9VwsLGuYpYqFSyYRHuU3rg/viewform?embedded=true"
+          :width="640"
+          height="1000"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class RegisterPage extends Vue {}
+export default class RegisterPage extends Vue {
+  get iframeWidth() {
+    switch (this.$vuetify.breakpoint.name) {
+      case 'xs': return '300'
+      case 'sm': return '540'
+        default: return '640'
+    }
+  }
+}
 </script>
