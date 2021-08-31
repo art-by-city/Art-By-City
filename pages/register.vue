fadae91ae96de2f6750d607646bff6297a0e98fa
Jim Toth <jim@meme.coach>
2021-08-31 20:06:42 +0000

    Resolve "google form for signup doesnt load"

--- pages/register.vue
+++ pages/register.vue
@@ -2,14 +2,56 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="auto">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSeBdtAoUMd5HY2K26Vq8VUdPIFL9VwsLGuYpYqFSyYRHuU3rg/viewform?embedded=true"
-          width="640"
-          height="1000"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-        ></iframe>
+        <span class="text-h2">Art By City Mailing List</span>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="auto" sm="6">
+        <p class="text-body-1">
+          Sign up for the Art By City Mailing List or join the Art By City
+          Discord Server to get notified about updates, including when we
+          launch Art By City accounts!
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <a
+          class="link-with-logo"
+          href="https://forms.gle/stQBR3HpS6oEJz4k6"
+          target="_blank"
+        >
+          <v-img
+            contain
+            max-height="72px"
+            src="/Forms_2020.svg"
+          ></v-img>
+          <span class="link-title">
+            Google Form Sign Up
+            <v-icon small dense class="adjust-icon">
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
+      </v-col>
+      <v-col cols="auto">
+        <a
+          class="link-with-logo"
+          href="https://discord.gg/SsHHQGSKTB"
+          target="_blank"
+        >
+          <v-img
+            contain
+            max-height="72px"
+            src="/Discord-Logo-Black.svg"
+          ></v-img>
+          <span class="link-title">
+            Art By City Discord
+            <v-icon small dense class="adjust-icon">
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
       </v-col>
     </v-row>
   </v-container>
@@ -21,3 +63,13 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class RegisterPage extends Vue {}
 </script>
+
+<style scoped>
+.adjust-icon {
+  margin-top: -3px;
+}
+.link-with-logo {
+  text-align: center;
+  display: inline-block;
+}
+</style>
