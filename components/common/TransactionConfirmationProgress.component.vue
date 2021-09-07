5b8aa75a16d446414dcf29567786c3e479de7da3
Jim Toth <jim@meme.coach>
2021-09-07 16:12:22 -0400

    Handle 202 and other improvements

--- components/common/TransactionConfirmationProgress.component.vue
+++ components/common/TransactionConfirmationProgress.component.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="transaction-confirmation-progress">
-    <span>Transaction found!  Waiting for confirmations...</span>
-    <v-progress-linear height="25" v-model="confirmationsPct">
+    <div class="font-italic">{{ txId }}</div>
+    <template v-if="status">
+      <strong>Status:&nbsp;</strong>
+      <span v-if="status === 404">Not found</span>
+      <span v-if="status === 202">Pending</span>
+      <span v-if="status === 200 && !isTxConfirmed">Confirming</span>
+      <span v-if="status === 200 && isTxConfirmed">Ready!</span>
+    </template>
+    <template v-else>
+      <span>Fetching tx...</span>
+    </template>
+    <v-progress-linear
+      v-if="status === 200"
+      height="25"
+      v-model="confirmationsPct"
+    >
       {{ confirmations }} / {{ $config.arweave.waitForConfirmations }}
     </v-progress-linear>
   </div>
@@ -24,6 +38,7 @@ export default class TransactionConfirmationProgress extends Vue {
   }) sleep!: number
 
   confirmations: number = 0
+  status: 404 | 202 | 200 | null = null
 
   private timeout?: number
 
@@ -33,6 +48,10 @@ export default class TransactionConfirmationProgress extends Vue {
 
   @Emit('notFound') onTransactionNotFound() {
     return this.txId
+  }
+
+  get isTxConfirmed(): boolean {
+    return this.confirmations >= this.$config.arweave.waitForConfirmations
   }
 
   get confirmationsPct(): number {
@@ -57,6 +76,7 @@ export default class TransactionConfirmationProgress extends Vue {
 
       if (txStatus.status === 200 && txStatus.confirmed) {
         this.confirmations = txStatus.confirmed.number_of_confirmations
+        this.status = 200
 
         const isConfirmed = this.confirmations >=
           this.$config.arweave.waitForConfirmations
@@ -66,7 +86,11 @@ export default class TransactionConfirmationProgress extends Vue {
         } else {
           this.checkTransactionStatus()
         }
+      } else if (txStatus.status === 202) {
+        this.status = 202
+        this.checkTransactionStatus()
       } else {
+        this.status = 404
         this.onTransactionNotFound()
       }
     }, this.sleep, this)
