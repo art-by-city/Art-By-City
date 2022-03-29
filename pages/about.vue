782d59db97466dc8ade3f37f792f84b36ef96b46
Jim Toth <jim@meme.coach>
2022-03-28 21:05:11 -0400

    wip

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,22 @@
 <template>
-  <v-container fluid style="width: 100%;">
-    <v-row justify="center">
-      <v-col cols="12" class="text-h2" style="text-align: center;">
-        <strong>Art By City Alpha</strong>
+  <v-container fluid class="pa-0" style="width: 100%;">
+    <v-row justify="center" align="center">
+      <v-col
+        cols="12"
+        class="text-h2 font-weight-bold text-center my-10"
+      >
+        <strong>The Artist's Permaweb</strong>
       </v-col>
     </v-row>
+
     <v-row justify="center">
-      <v-col cols="12" sm="6">
-        <p>
+      <v-col cols="12" md="6">
+        <p class="text-h5">
+          <b>Art By City</b> is a
+          <b>decentralized artist community &amp; protocol</b>
+        </p>
+
+        <!-- <p>
           <strong>Art By City</strong>
           is currently in <strong>Alpha Testing</strong>.  This dapp is our
           submission to the 5th run of the
@@ -21,11 +30,87 @@
               mdi-open-in-new
             </v-icon>
           </a> hackathon.
+        </p> -->
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec dui
+          nunc mattis enim ut tellus elementum. Pellentesque eu tincidunt tortor
+          aliquam nulla facilisi cras fermentum odio. Et odio pellentesque diam
+          volutpat commodo sed egestas egestas fringilla. Consequat semper
+          viverra nam libero. In hac habitasse platea dictumst quisque sagittis.
+          Donec adipiscing tristique risus nec feugiat in fermentum posuere
+          urna. Viverra ipsum nunc aliquet bibendum enim. Venenatis cras sed
+          felis eget velit aliquet. Enim sed faucibus turpis in eu mi bibendum.
+          Eget aliquet nibh praesent tristique magna. Erat imperdiet sed euismod
+          nisi porta lorem. Amet aliquam id diam maecenas ultricies. Gravida
+          neque convallis a cras. Vitae congue mauris rhoncus aenean vel elit
+          scelerisque mauris pellentesque. Amet purus gravida quis blandit
+          turpis cursus in hac habitasse. Cras ornare arcu dui vivamus arcu
+          felis bibendum. Mi tempus imperdiet nulla malesuada pellentesque
+          elit. Feugiat scelerisque varius morbi enim nunc faucibus a
+          pellentesque sit. Urna neque viverra justo nec ultrices dui sapien.
         </p>
         <p>
-          You may experience bugs or errors, so please bear with us as we
-          work to build <strong>The Artist's Permaweb</strong>.  Please
-          feel free to help us test the app, and provide us with feedback!
+          Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor.
+          Laoreet non curabitur gravida arcu ac tortor dignissim convallis
+          aenean. Nec nam aliquam sem et tortor consequat id. Consequat mauris
+          nunc congue nisi vitae suscipit. Viverra adipiscing at in tellus
+          integer feugiat scelerisque varius morbi. Suspendisse ultrices gravida
+          dictum fusce ut placerat orci. Semper viverra nam libero justo laoreet
+          sit amet cursus. Et odio pellentesque diam volutpat commodo sed
+          egestas egestas fringilla. Bibendum est ultricies integer quis auctor.
+          Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. Id
+          aliquet lectus proin nibh nisl condimentum. Proin sed libero enim sed
+          faucibus turpis. Sed faucibus turpis in eu mi bibendum.
+        </p>
+        <p>
+          Turpis massa tincidunt dui ut ornare lectus. Dolor morbi non arcu
+          risus quis. Tortor aliquam nulla facilisi cras fermentum odio. Morbi
+          non arcu risus quis varius. Proin nibh nisl condimentum id venenatis a
+          condimentum. Luctus accumsan tortor posuere ac ut. In est ante in nibh
+          mauris cursus mattis molestie. Suspendisse faucibus interdum posuere
+          lorem ipsum dolor sit amet. Vestibulum mattis ullamcorper velit sed.
+          Facilisis magna etiam tempor orci eu lobortis elementum nibh. Magna ac
+          placerat vestibulum lectus mauris ultrices eros in cursus. Hac
+          habitasse platea dictumst quisque sagittis. Ornare arcu odio ut sem
+          nulla pharetra diam sit amet. Lectus nulla at volutpat diam ut
+          venenatis tellus in metus. Quis imperdiet massa tincidunt nunc
+          pulvinar sapien et. In vitae turpis massa sed elementum tempus egestas
+          sed. Odio eu feugiat pretium nibh ipsum consequat nisl vel. Semper
+          auctor neque vitae tempus. Tempor id eu nisl nunc mi ipsum faucibus
+          vitae aliquet. Aenean vel elit scelerisque mauris pellentesque
+          pulvinar pellentesque.
+        </p>
+        <p>
+          Id ornare arcu odio ut sem nulla pharetra diam sit. Sagittis vitae et
+          leo duis ut diam. Vitae elementum curabitur vitae nunc sed. Felis eget
+          velit aliquet sagittis id consectetur purus ut faucibus. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. Dui
+          nunc mattis enim ut. Id leo in vitae turpis massa sed elementum tempus
+          egestas. Fermentum leo vel orci porta non pulvinar. Ut consequat
+          semper viverra nam libero justo laoreet. Arcu bibendum at varius vel
+          pharetra vel. Et netus et malesuada fames ac turpis. Tortor at auctor
+          urna nunc id cursus metus aliquam eleifend. Sit amet consectetur
+          adipiscing elit. Pulvinar elementum integer enim neque volutpat. Et
+          magnis dis parturient montes. Et molestie ac feugiat sed lectus
+          vestibulum mattis. Tortor pretium viverra suspendisse potenti nullam
+          ac. Gravida in fermentum et sollicitudin ac orci phasellus egestas.
+          Dictum varius duis at consectetur lorem donec massa. Et egestas quis
+          ipsum suspendisse ultrices gravida dictum.
+        </p>
+        <p>
+          Id leo in vitae turpis massa sed elementum. Tincidunt ornare massa
+          eget egestas purus viverra accumsan in nisl. Lobortis mattis aliquam
+          faucibus purus in massa. Feugiat pretium nibh ipsum consequat. Laoreet
+          non curabitur gravida arcu ac tortor dignissim convallis aenean.
+          Tincidunt dui ut ornare lectus sit amet. Commodo ullamcorper a lacus
+          vestibulum sed arcu non odio euismod. Enim sed faucibus turpis in.
+          Tincidunt dui ut ornare lectus sit amet est placerat. Massa id neque
+          aliquam vestibulum morbi blandit cursus. Lobortis elementum nibh
+          tellus molestie nunc non. Et magnis dis parturient montes nascetur.
+          Nibh mauris cursus mattis molestie a iaculis at erat pellentesque.
+          Convallis posuere morbi leo urna molestie at.
         </p>
       </v-col>
     </v-row>
@@ -38,3 +123,9 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class AboutPage extends Vue {}
 </script>
+
+<style scoped>
+.top-banner >>> .v-parallax__content {
+  background-color: rgba(0, 0, 0, 0.333);
+}
+</style>
---
+++ pages/faq.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid style="width: 100%;">
+    <v-row justify="center">
+      <v-col cols="12" class="text-h2" style="text-align: center;">
+        <strong>FAQ</strong>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class FaqPage extends Vue {}
+</script>
