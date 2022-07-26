cfe03abcbfe827fb575d5e9d4d21f38f31215a52
Jim Toth <jim@meme.coach>
2022-07-26 20:59:31 +0000

    Update generate secrets for db

--- generateSecrets.js
+++ generateSecrets.js
@@ -4,5 +4,10 @@ console.log(
   JWT_EXPIRES_IN: ${process.env.JWT_EXPIRES_IN}
   SMTP_HOST: ${process.env.SMTP_HOST}
   SMTP_USER: ${process.env.SMTP_USER}
-  SMTP_PASS: ${process.env.SMTP_PASS}`
+  SMTP_PASS: ${process.env.SMTP_PASS}
+  DB_HOST: ${process.env.DB_HOST}
+  DB_PORT: ${process.env.DB_PORT}
+  DB_USER: ${process.env.DB_USER}
+  DB_PASS: ${process.env.DB_PASS}
+  DB_NAME: ${process.env.DB_NAME}`
 )
