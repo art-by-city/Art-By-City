7d0dc66c558f82ab8985042b544acdba71519621
Jim Toth <jim@meme.coach>
2022-08-19 11:58:30 +0000

    Resolve "page loading splash transition"

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
    ** Customize the progress-bar color
    */
   loading: {
-    color: 'blue',
+    color: 'white',
     height: '5px',
     duration: 10000,
     continuous: true,
