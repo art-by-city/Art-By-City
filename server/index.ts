baace9cd0c5bd0557f613d0aa00dd20d42b150f8
Jim Toth <jim@artby.city>
2023-01-05 21:45:36 +0000

    Fix abc node use admin jwk

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,11 @@ import fs from 'fs'
 
 let adminJWK
 try {
-  const adminJWKPath = process.env.ADMIN_JWK_PATH || 'ADMIN_JWK_PATH not set!'
+  const adminJWKPath = process.env.ADMIN_JWK || 'ADMIN_JWK not set!'
+  console.log('[ArtByCityNode Middleware] ADMIN_JWK', adminJWKPath)
   const adminJWKBuffer = fs.readFileSync(adminJWKPath)
   adminJWK = JSON.parse(adminJWKBuffer.toString())
+  console.log('[ArtByCityNode Middleware] json ADMIN_JWK', adminJWK)
 } catch (error) {
   console.error(
     '[ArtByCityNode Middleware] Error bootstrapping ArtByCityNode',
