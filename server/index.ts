face0e95754d19a00e46eaf023ab86047f7980c5
Jim Toth <jim@artby.city>
2023-01-05 19:23:21 +0000

    Resolve "fix node middleware admin jwk"

--- server/index.ts
+++ server/index.ts
@@ -3,16 +3,9 @@ import fs from 'fs'
 
 let adminJWK
 try {
-  console.log('[ArtByCityNode Middleware] EXM_API_KEY', process.env.EXM_API_KEY)
-  console.log(
-    '[ArtByCityNode Middleware] ARK_EXM_FUNCTION_ID',
-    process.env.ARK_EXM_FUNCTION_ID
-  )
   const adminJWKPath = process.env.ADMIN_JWK_PATH || 'ADMIN_JWK_PATH not set!'
-  console.log('[ArtByCityNode Middleware] ADMIN_JWK_PATH', adminJWKPath)
   const adminJWKBuffer = fs.readFileSync(adminJWKPath)
   adminJWK = JSON.parse(adminJWKBuffer.toString())
-  console.log('[ArtByCityNode Middleware] ADMIN_JWK', adminJWK)
 } catch (error) {
   console.error(
     '[ArtByCityNode Middleware] Error bootstrapping ArtByCityNode',
