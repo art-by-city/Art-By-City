b3e8008e0c25a9f96b0ccea3e913b89a651d5847
Jim Toth <jim@artby.city>
2023-01-04 22:13:41 -0500

    debug abc node middlewares

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,15 @@ import fs from 'fs'
 let adminJWK
 try {
   const adminJWKPath = process.env.ADMIN_JWK_PATH || 'ADMIN_JWK_PATH not set!'
+  console.log('[ArtByCityNode Middleware] ADMIN_JWK_PATH', adminJWKPath)
   const adminJWKBuffer = fs.readFileSync(adminJWKPath)
   adminJWK = JSON.parse(adminJWKBuffer.toString())
+  console.log('[ArtByCityNode Middleware] ADMIN_JWK', adminJWK)
 } catch (error) {
-  console.error('Error bootstrapping ArtByCityNode', error)
+  console.error(
+    '[ArtByCityNode Middleware] Error bootstrapping ArtByCityNode',
+    error
+  )
 }
 
 const abc = new ArtByCityNode(adminJWK)
