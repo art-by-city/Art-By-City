f0311e681ae952fab72026889274d2e8619aca73
Jim Toth <jim@artby.city>
2023-01-05 00:49:47 -0500

    fix abc node secrets

--- generateSecrets.js
+++ generateSecrets.js
@@ -9,5 +9,9 @@ console.log(
   DB_PORT: ${process.env.DB_PORT}
   DB_USER: ${process.env.DB_USER}
   DB_PASS: ${process.env.DB_PASS}
-  DB_NAME: ${process.env.DB_NAME}`
+  DB_NAME: ${process.env.DB_NAME}
+  EXM_API_KEY: ${process.env.EXM_API_KEY}
+  ARK_EXM_FUNCTION_ID: ${process.env.ARK_EXM_FUNCTION_ID}
+  ADMIN_JWK_PATH: ${process.env.ADMIN_JWK_PATH}
+  `
 )
