91cec5f29a2e3181a8402ac87bdaa661d5419303
Jim Toth <jim@artby.city>
2023-01-05 00:31:57 -0500

    try some debugging stuff

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,18 @@
 import ArtByCityNode from '@artbycity/node'
 import fs from 'fs'
 
+import { CONFIG } from '@artbycity/node/dist/util'
+
 let adminJWK
 try {
-  console.log('[ArtByCityNode Middleware] EXM_API_KEY', process.env.EXM_API_KEY)
+  console.log(
+    '[ArtByCityNode Middleware] USER_UPLOAD_BUCKET_NAME',
+    process.env.USER_UPLOAD_BUCKET_NAME
+  )
+  console.log('[ArtByCityNode Middleware] EXM_API_KEY', CONFIG.EXM_API_KEY)
   console.log(
     '[ArtByCityNode Middleware] ARK_EXM_FUNCTION_ID',
-    process.env.ARK_EXM_FUNCTION_ID
+    CONFIG.ARK_EXM_FUNCTION_ID
   )
   const adminJWKPath = process.env.ADMIN_JWK_PATH || 'ADMIN_JWK_PATH not set!'
   console.log('[ArtByCityNode Middleware] ADMIN_JWK_PATH', adminJWKPath)
