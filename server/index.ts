48ec86da7315a83500785f67693796e3fe38b14f
Jim Toth <jim@artby.city>
2023-01-05 14:52:53 -0500

    fixxxxxxx

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,10 @@ import fs from 'fs'
 let adminJWK
 try {
   const adminJWKPath = process.env.ADMIN_JWK_PATH || 'ADMIN_JWK_PATH not set!'
+  console.log('[ArtByCityNode Middleware] ADMIN_JWK_PATH', adminJWKPath)
   const adminJWKBuffer = fs.readFileSync(adminJWKPath)
   adminJWK = JSON.parse(adminJWKBuffer.toString())
+  console.log('[ArtByCityNode Middleware] ADMIN_JWK', adminJWK)
 } catch (error) {
   console.error(
     '[ArtByCityNode Middleware] Error bootstrapping ArtByCityNode',
